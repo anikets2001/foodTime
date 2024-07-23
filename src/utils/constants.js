811b39533137c49ffd60e3073df12115419d473c
Aniket Singh <aniketsingh90126@gmail.com>
2024-07-23 14:36:43 +0530

    erroPage done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
-export const cardListBaseUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const CARD_LIST_BASE_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const cardImageBaseURL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CARD_IMAGE_BASE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All";
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All";
+
+export const ERROR_IMG =
+  "https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg";
