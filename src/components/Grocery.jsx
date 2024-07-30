0d6b31e846e63217c5e557ef19bb0ef13cd3a770
Aniket Singh <aniketsingh90126@gmail.com>
2024-07-30 14:35:48 +0530

    feat: implemented lazy loading

---
+++ src/components/Grocery.jsx
@@ -0,0 +1,10 @@
+const Grocery = () => {
+  return (
+    <div>
+      our grocery online store and we have a lot of child components inside this
+      webpage
+    </div>
+  );
+};
+
+export default Grocery
